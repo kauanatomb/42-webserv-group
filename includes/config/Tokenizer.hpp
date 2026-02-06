bf773c8df1b9694651b38e0005632563106ab517
fa10-c <personal.fatin@gmail.com>
2026-02-06 18:02:31 +0100

    Added Tokenizer interface

--- includes/config/Tokenizer.hpp
+++ includes/config/Tokenizer.hpp
@@ -1,1 +1,26 @@
 // tokenizer class
+
+#pragma once
+#include <vector>
+#include <istream>
+#include "Token.hpp"
+
+class Tokenizer
+{
+    public:
+        explicit Tokenizer(std::istream& in);
+        std::vector<Token> tokenizer();
+
+        static void printTokens(const std::vector<Token>& tokens);
+    private:
+        std::istream& _in;
+        int           _line;
+        int           _col;
+
+        int peekChar();
+        int getChar();
+        void skipWhitespace();
+        bool isDelimiter(int ch) const;
+        Token makeSymbolToken(int ch, int line, int col);
+    
+};
