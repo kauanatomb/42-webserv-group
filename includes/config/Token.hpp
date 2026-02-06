0c5d22767f0ee65b8810784ef2e92a49857e7950
fa10-c <personal.fatin@gmail.com>
2026-02-06 17:42:28 +0100

    Added Token definition for config tokenizer

--- includes/config/Token.hpp
+++ includes/config/Token.hpp
@@ -1,1 +1,21 @@
-// Token enum and Token struct
+#pragma once
+#include <string>
+
+enum TokenType {
+    TOK_WORD,
+    TOK_LBRACE,     // {
+    TOK_RBRACE,     // }
+    TOK_SEMICOLON,  // ;
+    TOK_END         // EOF
+};
+
+struct Token {
+    TokenType   type;
+    std::string value;
+    int         line;
+    int         col;
+
+    Token() : type(TOK_END), value(""), line(1), col(1) {}
+    Token(TokenType t, const std::string& v, int l, int c)
+        : type(t), value(v), line(l), col(c) {}
+};
