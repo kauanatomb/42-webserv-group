ba75db4c841ab9abd72ee3186e8bdc6377f762a1
fa10-c <personal.fatin@gmail.com>
2026-02-26 15:33:15 +0100

    "fixed ResponseBuilder::ok sig mismatch"

--- includes/httpCore/ResponseBuilder.hpp
+++ includes/httpCore/ResponseBuilder.hpp
@@ -6,6 +6,8 @@
 
 class ResponseBuilder {
     public:
-        static HttpResponse ok(const std::string& body, const std::string& contentType);
+        HttpResponse ResponseBuilder::ok(const std::string& body, const std::string& path);
+    //missmatch with the implementation, so i'm sticking to the prototype of the function implemented
+    //    static HttpResponse ok(const std::string& body, const std::string& contentType);
         static HttpResponse redirect(int code, const std::string& location);
 };
